378e311949f53849fec8f945a093bf2b1e08818b
zeenayouhan <zinayouhan33@gmail.com>
2020-03-11 17:44:02 +0530

    Add Component

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import TodoInput from './Component/TodoInput';
+import TodoList from './Component/TodoList';
+import TodoItem from './Component/TodoItem';
+
 
 function App() {
   return (
     <div >
-      
+      Hi Baby!!!
+      <TodoInput/>
+      <TodoList/>
+      <TodoItem/>
     </div>
   );
 }
