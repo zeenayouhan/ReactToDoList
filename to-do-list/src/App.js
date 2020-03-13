22a50deec4f25fa25cc1bd38e92317cf89098340
zeenayouhan <zinayouhan33@gmail.com>
2020-03-13 14:36:05 +0530

    todoinputstate

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component } from 'react';
 import './App.css';
 import TodoInput from './Component/TodoInput';
 import TodoList from './Component/TodoList';
@@ -6,16 +6,42 @@ import TodoList from './Component/TodoList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import uuid from "uuid";
 
+class App extends Component{
+  state={
+    items:[],
+    id:0,
+    item:'',
+    editItem: false
+  }
+  handleSubmit=(e)=>{
+    e.preventDefault();
 
-function App() {
-  return (
-    
+
+    const newItem={
+      id:this.state.id,
+      item:this.state.item
+    }
+
+    const updatedItems=[...this.state.items,newItem]
+
+  }
+  handleChange=e=>{
+    this.setState({
+      item: e.target.value
+    });
+  };
+  render() {
+    return (  
     
     <div className="container">
       <div className="row">
         <div className="col-10 mx-auto col-md-8 mt-4">
         <h3 className="text-capitalize text-center">To do Input</h3>
-        <TodoInput/>
+        <TodoInput
+        item={this.state.item} 
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+        />
         <TodoList/>
         
         </div>
@@ -24,6 +50,7 @@ function App() {
     </div>
     
   );
+}
 }
 
 export default App;
--- to-do-list/src/Component/TodoInput.js
+++ to-do-list/src/Component/TodoInput.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react'
 
 export default class TodoInput extends Component {
     render() {
+        const{item,handleChange, handleSubmit}=this.props;
         return (
             <div className="card card-body my-3">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="input=-group">
                         <div className="input-group-prepend">
                             <div className="input-group-text bg-primary text-white">
                                 <i className="fas fa-book"/>
 
                             </div>
-                            <input type="text"className="form-control text-capitalize" placeholder="add a todo item"/>
+                            <input 
+                            type="text" 
+                            className="form-control text-capitalize" 
+                            placeholder="add a todo item"
+                            value={item}
+                            onChange={handleChange}/>
                         </div>
                         <button type="submit" className="btn btn-block btn-primary mt-3">Add Item</button>
 
