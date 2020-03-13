5723e38497c8e953fb67d0c66c4139adcb1ca4de
zeenayouhan <zinayouhan33@gmail.com>
2020-03-13 21:09:25 +0530

    additem1

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -4,12 +4,13 @@ import TodoInput from './Component/TodoInput';
 import TodoList from './Component/TodoList';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import uuid from "uuid";
+
+ const uuid= require('uuid/v1')
 
 class App extends Component{
   state={
     items:[],
-    id:0,
+    id:uuid(),
     item:'',
     editItem: false
   }
@@ -21,8 +22,16 @@ class App extends Component{
       id:this.state.id,
       item:this.state.item
     }
+    console.log(newItem);
 
     const updatedItems=[...this.state.items,newItem]
+
+    this.setState({
+      items:updatedItems,
+      item:'', 
+      id:uuid(),
+      editItem: false
+    })
 
   }
   handleChange=e=>{
@@ -42,7 +51,7 @@ class App extends Component{
         handleChange={this.handleChange}
         handleSubmit={this.handleSubmit}
         />
-        <TodoList/>
+        <TodoList items={this.state.items}/>
         
         </div>
         
--- to-do-list/src/Component/TodoList.js
+++ to-do-list/src/Component/TodoList.js
@@ -3,11 +3,17 @@ import TodoItem from './TodoItem'
 
 export default class TodoList extends Component {
     render() {
+        const{items}=this.props
         return (
             <div>
                 <ul className="list-group my-5">
                     <h3 className="text-capitalize text-center">todo list</h3>
-                    <TodoItem/>
+                    {items.map(item=>{
+                        return(
+                            <TodoItem key={item.id} title={item.title}/>
+                        )
+                        })
+                }
                     <button type="button" className="btn btn-danger mt-3 btn-block text-capitalize">clear list</button>
                 </ul>
             </div>
